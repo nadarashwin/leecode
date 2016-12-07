d696b096a8a3f3fd31704e14b15f71ea5df5e797
Ashwin Balakrishnan <nadarashwin@gmail.com>
2016-12-07 16:53:25 +0530

    created a single function to print out the requirement  for Two_sum

--- Two_Sum.py
+++ Two_Sum.py
@@ -8,19 +8,14 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 first should = return [0, 1]
 second should = return 2 7 
 '''
+nums = [2, 7, 11, 15]
+target = 9
 
 dic = {}
 for i, num in enumerate(nums):
     if num in dic:
-        print [dic[num], i]
-    else:
-        dic[target - num] = i
-
-
-dic = {}
-for i, num in enumerate(nums):
-    if num in dic:
-        print nums[dic[num]], nums[i]
+        print [dic[num], i]   ## printing the index
+	print nums[dic[num]], nums[i] ## printing the actual element
     else:
         dic[target - num] = i
 
